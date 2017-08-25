68d6e24a25574b71cdf77c3faeb4fbdc5918dd6f
AN-SBP\allen <allennikka@gmail.com>
2017-08-25 12:21:56 -0700

    Successfully recieveing and parsing all data

--- nodeTest.js
+++ nodeTest.js
@@ -11,13 +11,13 @@ var postHTML =
   '<ul class="dataList"> </ul>' +
   '</body></html>';
 
-  var cheerio = require('cheerio'),
-      $ = cheerio.load('<h3 class = "title">Hello world</h3>');
+var cheerio = require('cheerio'),
+  $ = cheerio.load('<h3 class = "title">Hello world</h3>');
 
-  $('h3.title').text('Hello there!');
-  $('h3').addClass('welcome');
+$('h3.title').text('Hello there!');
+$('h3').addClass('welcome');
 
-  $.html();
+$.html();
 
 // var particleSRC = '//cdn.jsdelivr.net/particle-api-js/5/particle.min.js';
 // $.getScript(particleSRC, function() {
@@ -47,9 +47,27 @@ http.createServer(function(req, res) {
     body += chunk;
   });
   req.on('end', function() {
-    console.log('POSTed: ' + body);
+    var patt = /event=send-i%2Cv%2Cpf%2Cs%2Cp%2Cq&data=/;
+    if (patt.test(body)) {
+      // var dataArray = body.match(/[2C|data=][0-9]*[.][0-9]*[%]/g);
+      var dataArray = body.match(/[0-9]*[.][0-9]*/g);
+      var labelArray = ["current", "voltage", "power factor",
+        "apparent power", "real power", "reactive power", "error"
+      ]
+      for (i = 0; i < dataArray.length - 1; i++) {
+        // dataArray[i].replace(/[=]/g,"");
+        // dataArray[i].replace(/[C]/g,"");
+        console.log(labelArray[i] + '= ' + dataArray[i]);
+      }
+      var dateStamp = body.match(/[0-9]{4}[-][0-9]{2}[-][0-9]{2}/)
+      var timeStamp = body.match(/[0-9]{2}%3A[0-9]{2}%3A[0-9]{2}/)
+      var newTimeStamp = timeStamp[0].replace(/%3A/g,":")
+      console.log(newTimeStamp + " " + dateStamp[0])
+    }
+    console.log('Original: ' + body)
     res.writeHead(200);
     res.end(postHTML);
+
     // $(body).insertBefore(".dataList") //FIXME
   });
 }).listen(3456);
